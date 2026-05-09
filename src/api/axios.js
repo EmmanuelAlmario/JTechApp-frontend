aa70669ac9355ec2d969005a6c3945b14631780b
Emmanuel <emanuelalmarionovoa@gmail.com>
2026-05-09 18:24:59 -0500

    added the env configuration to deploy the frontend

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,19 +1,18 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080',
+  baseURL: import.meta.env.VITE_API_URL
 });
 
 api.interceptors.request.use(
   (config) => {
-    const token = localStorage.getItem('token'); 
+    const token = localStorage.getItem('token');
     if (token) {
-      config.headers.Authorization = `Bearer ${token}`;    }
+      config.headers.Authorization = `Bearer ${token}`;
+    }
     return config;
   },
-  (error) => {
-    return Promise.reject(error);
-  }
+  (error) => Promise.reject(error)
 );
 
 export default api;
