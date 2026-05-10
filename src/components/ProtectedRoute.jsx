ff1f9f2e3c7d97c1c2bae8dfb53d55df0840257e
Emmanuel <emanuelalmarionovoa@gmail.com>
2026-05-09 19:12:15 -0500

    Fixed another thing in axios

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -4,11 +4,11 @@ import { useAuth } from '../context/AuthContext'
 export default function ProtectedRoute({ children, roles }) {
   const { user } = useAuth()
 
-  if (!usuario) {
+  if (!user) {
     return <Navigate to="/login" />
   }
 
-  if (roles && !roles.includes(usuario.rol)) {
+  if (roles && !roles.includes(user.rol)) {
     return <Navigate to="/" />
   }
 
